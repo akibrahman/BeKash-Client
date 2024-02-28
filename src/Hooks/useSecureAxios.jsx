00fccdc4ae5e2ddb383838c101103651c1ec06bb
akibrahman <akibrahman5200@gmail.com>
2024-02-28 08:28:17 +0600

    Axios problem solved

--- src/Hooks/useSecureAxios.jsx
+++ src/Hooks/useSecureAxios.jsx
@@ -1,15 +1,11 @@
 import axios from "axios";
-import { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../Providers/AuthProvider";
+import { useEffect } from "react";
 
 const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_SERVER_URL}`,
   withCredentials: true,
 });
 const useSecureAxios = () => {
-  const { logOut } = useContext(AuthContext);
-  const navigate = useNavigate();
   //!
   useEffect(() => {
     axiosInstance.interceptors.response.use(
@@ -18,14 +14,14 @@ const useSecureAxios = () => {
       },
       (error) => {
         // console.log("Tracked in the Axios Interceptor - ", error.response);
-        if (error.response.status === 401 || error.response.status === 403) {
-          console.log("Problem with Token - ", error.response.status);
-          logOut()
-            .then(() => {
-              navigate("/login");
-            })
-            .catch();
-        }
+        // if (error.response.status === 401 || error.response.status === 403) {
+        //   console.log("Problem with Token - ", error.response.status);
+        //   logOut()
+        //     .then(() => {
+        //       navigate("/login");
+        //     })
+        //     .catch();
+        // }
         return error;
       }
     );
